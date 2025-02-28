a1a2e57c8ebfc02516de5c5129988d80be7d9c38
JoJoeun <ejqmfzpdlz@naver.com>
2025-03-01 06:10:08 +0900

    feat: comment UI

---
+++ PlanIt/src/constants/menus.jsx
@@ -0,0 +1,11 @@
+export const userMenuItems = [
+    { label: "프로필 조회", onClick: () => console.log("프로필 조회 클릭됨") },
+    { label: "채팅하기", onClick: () => console.log("채팅하기 클릭됨") },
+    { label: "신고하기", onClick: () => console.log("신고하기 클릭됨") },
+    { label: "차단하기", onClick: () => console.log("차단하기 클릭됨") },
+];
+  
+export const moreMenuItems = [
+    { label: "편집하기", onClick: () => console.log("편집하기 클릭됨") },
+    { label: "삭제하기", onClick: () => console.log("삭제하기 클릭됨") },
+];
